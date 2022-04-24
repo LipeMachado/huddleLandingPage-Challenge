84520d24b4a146b1639f8225e0703b72656080a8
LipeMachado <silvalipe765@gmail.com>
2022-04-24 19:38:03 -0300

    Background Fixed

--- src/GlobalStyle/styledGlobal.jsx
+++ src/GlobalStyle/styledGlobal.jsx
@@ -12,5 +12,6 @@ export const GlobalStyle = createGlobalStyle`
         background-image: url(${backgroundDesktop});
         background-size: cover;
         background-repeat: no-repeat;
+        height: 100vh;
     }
 `;
