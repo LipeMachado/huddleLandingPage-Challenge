c91aab067598f030ab1c13adadd93a55cded932c
LipeMachado <silvalipe765@gmail.com>
2022-04-24 02:53:07 -0300

    Background

--- src/Pages/InitialPage/index.jsx
+++ src/Pages/InitialPage/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {
-
+    BackgroundSection
 } from './style.jsx'
 
 export function InitialPage() {
     return (
         <>
-            
+            <BackgroundSection />
         </>
     );
 }
--- src/Pages/InitialPage/style.jsx
+++ src/Pages/InitialPage/style.jsx
@@ -1,2 +1,11 @@
 import styled from 'styled-components'
+import backgroundDesktop from '../../Images/bg-desktop.svg'
 
+export const BackgroundSection = styled.section`
+    background-image: url(${backgroundDesktop});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    z-index: 1;
+    height: 100vh;
+`;
