47e2047ec96cdaa78d6fd74fab01139eb047485e
LipeMachado <silvalipe765@gmail.com>
2022-07-12 17:47:01 -0300

    Fix StyleGlobal

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { InitialPage } from './Pages/InitialPage/'
-import { GlobalStyle } from './styledGlobal.jsx'
+import { GlobalStyle } from './Pages/InitialPage/styledGlobal.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
